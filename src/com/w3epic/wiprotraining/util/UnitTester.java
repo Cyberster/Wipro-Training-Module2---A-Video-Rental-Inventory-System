c4f40476db7807324e26b7dfcf994e89109fada6
Arpan Das <arp14@yahoo.com>
2018-08-19 12:32:53 +0530

    JUnit mini project 1: Using JUnit, test the mini project that you have created in OOPS/Inheritance Tech module. - All test classes are added

---
+++ src/com/w3epic/wiprotraining/util/UnitTester.java
@@ -0,0 +1,22 @@
+package com.w3epic.wiprotraining.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.w3epic.wiprotraining.service.Video;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	VideoTest.class,
+	VideoStoreTest.class
+})
+
+public class UnitTester {
+	
+	public class AllTests {
+		
+	}
+}
---
+++ src/com/w3epic/wiprotraining/util/VideoTest.java
@@ -0,0 +1,61 @@
+package com.w3epic.wiprotraining.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.w3epic.wiprotraining.service.Video;
+
+public class VideoTest {
+	private Video video;
+	private String videoName;
+	//private boolean checkout;
+	private int rating;
+	
+	@Before
+	public void before() {
+		videoName = "Test Name";
+		//checkout = true;
+		rating = 5;
+		video = new Video(videoName);
+	}
+
+	@Test
+	public void getNameTest() {
+		assertEquals(videoName, video.getName());
+	}
+	
+	@Test
+	public void doCheckoutTest() {
+		video.doCheckout();
+		assertTrue(video.getCheckout());
+	} 
+	
+	@Test
+	public void doReturnTest() {
+		video.doReturn();
+		assertFalse(video.getCheckout());
+	}
+	
+	@Test
+	public void receiveRatingTest() {
+		video.receiveRating(rating);
+		assertEquals(rating, video.getRating());
+	}
+	
+	@Test
+	public void getRatingTest() {
+		video.receiveRating(rating);
+		assertEquals(rating, video.getRating());
+	}
+	
+	@Test
+	public void getCheckoutTest() {
+		video.doCheckout();
+		assertTrue(video.getCheckout());
+		video.doReturn();
+		assertFalse(video.getCheckout());
+	}
+
+}
